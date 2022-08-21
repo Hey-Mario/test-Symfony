908e9b24587dde42e1ec3bcc025f3a8d306c1452
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-08-21 07:18:35 +0300

    nanao an'ilay resaka route tao amin'ny chapitre 5

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,11 +4,26 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController{
 
+    // Ajout des landings pages avec le Response
     #[Route('/')]
-    public function homepage(){
-        return new Response('Something else for the test');
+    public function homepage(): Response
+    {
+        return new Response('Our landing page');
     }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug){
+            $title = 'Genre : '.u(str_replace('-', ' ', $slug))->title(true);
+        } else{
+            $title = 'All Genre';
+        }
+        return new Response($title);
+    }
+
 }
